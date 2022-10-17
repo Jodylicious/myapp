45050883438b24897df9d856a3d826377fffa11b
Jodylicious <jb418729@gmail.com>
2022-10-17 17:14:02 +0800

    first

--- src/components/CourseDetails/BSPSY.jsx
+++ src/components/CourseDetails/BSPSY.jsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
 
 
 export default function CourseCard() {
@@ -19,14 +17,8 @@ export default function CourseCard() {
                     image="https://www.mooc.org/hubfs/psych-fields-jpg.jpeg"
                     alt="BSPSY"
                     height="150" />
-
                 {/* This is the course definition */}
-                <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                   It is designed to prepare students towards a scientific understanding of human behavior. Students will have the experiences in psychological evaluation, research work and community involvement. It also prepares the student for graduate work.
-                    </Typography>
-
-                </CardContent>
+          
                
             </Card>
         </div>
