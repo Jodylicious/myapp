0b39fd34da0ad8fb7df605f38c8d2a718c8471ed
Jodylicious <jb418729@gmail.com>
2022-10-17 17:07:15 +0800

    change ulit

--- src/components/CourseDetails/BSPSY.jsx
+++ src/components/CourseDetails/BSPSY.jsx
@@ -1,32 +1,11 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-
-const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-}));
 
 export default function CourseCard() {
-    const [expanded, setExpanded] = React.useState(false);
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
-
     return (
         <div className="cardsss">
             <Card style={{ maxWidth: '100' }}>
@@ -48,27 +27,7 @@ export default function CourseCard() {
                     </Typography>
 
                 </CardContent>
-                {/* This is other information section (collapsed) */}
-                <CardActions disableSpacing >
-                    <h5 className='moreInfo'>Other Information</h5>
-                    <ExpandMore
-                        expand={expanded}
-                        onClick={handleExpandClick}
-                        aria-expanded={expanded}
-                        aria-label="show more">
-                        <ExpandMoreIcon />
-                    </ExpandMore>
-                </CardActions>
-
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                        <Typography className="courseInfo">
-                            <div className='maintenance'>
-                            <h4>Sorry this course page is under construction. We apologize for the inconvenience.</h4>
-                            </div>
-                        </Typography>
-                    </CardContent>
-                </Collapse>
+               
             </Card>
         </div>
     );
